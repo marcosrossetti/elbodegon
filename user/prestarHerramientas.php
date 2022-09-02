2e36ee8979745b96fe0f6ef330131acc93647ab0
rodrigoacunaa <rodrigosebastianok@gmail.com>
2022-09-01 23:23:42 -0300

    agregado de funcionalidad de prestado de herramientas

--- user/prestarHerramientas.php
+++ user/prestarHerramientas.php
@@ -66,7 +66,7 @@ session_start();
                                     <h5 class="card-title fw-bold">'.$nombre.'</h5>
 
                                     <p>Cantidad '.$cantidad.'</p>
-                                    <a class="btn btn-outline-dark mt-auto" href="modules/prestarMod.php?id='.$id.'">Agregar</a>
+                                    <a class="btn btn-outline-dark mt-auto" id="agregarBtn" data-nombre="'.$nombre.'" data-id="'.$id.'">Agregar</a>
                                 </div>
                             </div>
                         </div>
@@ -82,29 +82,12 @@ session_start();
                     <ul class="list-group">
                         <!--<a class="list-group-item" href="#">Ver todos los productos</a>-->
                         <!--<a class="list-group-item" href="#">Retirar productos</a>-->
-                        <div class="row g-0">
-                            <li class="list-group-item">Destornillador Phillips PH-1</li>
-                        </div>
-                        <div class="row g-0 mb-3">
-                            <div class="btn-group btn-group-sm" role="group" aria-label="btnCantidad"> <!-- col-3 -->
-                                <button type="button" onclick="$('.cantidad').html() <= 1 ? 1 : $('.cantidad').html(parseInt($('.cantidad').html()) - 1);" class="btn btn-dark"><i class="fas fa-minus"></i></button>
-                                <button type="button" class="btn btn-dark cantidad">1</button>
-                                <button type="button" onclick="$('.cantidad').html(parseInt($('.cantidad').html()) + 1);" class="btn btn-dark"><i class="fas fa-plus"></i></button>
-                                <button type="button" class="btn btn-danger"><i class="fas fa-times"></i></button>
-                            </div>
-                        </div>
 
-                        <div class="row g-0">
-                            <li class="list-group-item">Sargento</li>
-                        </div>
-                        <div class="row g-0 mb-3">
-                            <div class="btn-group btn-group-sm" role="group" aria-label="btnCantidad"> <!-- col-3 -->
-                                <button type="button" onclick="$('.cantidad2').html() <= 1 ? 1 : $('.cantidad2').html(parseInt($('.cantidad2').html()) - 1);" class="btn btn-dark"><i class="fas fa-minus"></i></button>
-                                <button type="button" class="btn btn-dark cantidad2">1</button>
-                                <button type="button" onclick="$('.cantidad2').html(parseInt($('.cantidad2').html()) + 1);" class="btn btn-dark"><i class="fas fa-plus"></i></button>
-                                <button type="button" class="btn btn-danger"><i class="fas fa-times"></i></button>
-                            </div>
-                        </div>
+                       <div id="contCont">
+                           
+                       </div>
+                        
+                        
 
                     </ul>
                     <button class="btn btn-success w-100">Enviar</button>
@@ -119,5 +102,138 @@ session_start();
         <!-- Bootstrap core JS-->
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+
+        <script>
+            var contenidos = [];
+            var ids = [];
+            var cant = [];
+
+             $(document).on('click', '#agregarBtn',function () {
+
+
+                 let nombre = $(this).data('nombre');
+                let id = $(this).data('id');
+                console.log(id , nombre);
+
+                if(ids.length == 0){
+                    addCart();
+                }
+
+                for(let i = 0; i < ids.length; i++){
+
+
+                    if(ids[i] == id){
+                        console.log("break");
+                        break;
+                    }else{
+                        if(i == ids.length-1){
+                            addCart();
+
+                        }
+                    }
+                }
+
+
+
+                
+                
+
+
+
+                // $('#formadorForm').submit(e => {
+                //     e.preventDefault();
+                //     //creacion de objeto de almacenamiento de los inputs "postData"
+                //     const postData = {
+                //     //guardamos los input dentro de un objeto
+                //     nuevoFor : $("#nuevoFor").val(),
+                //     id : $("#idFormador").val()
+                //     };
+                //     //validacion ternaria de redireccion segun valor de la variable booleana "edit"
+                //     const url = "modulos/modCursos/editarFor.php";
+                //     //mostramos por pantalla el objeto y la direccion donde sera enviada para ser procesado
+                //     console.log(postData, url);
+                //     //metodo post por jquery parametros = (direccion url del archivo php, el objeto que guarda los datos a procesar, una funcion de respuesta al
+                //     //procesamiento de dichos datos)
+                //     $.post(url, postData, (response) => {
+
+                //     const rta = JSON.parse(response);
+                //     console.log(rta);
+                //     if(rta == 1){
+                //         window.location = "administrarCursos.php";
+                //     }
+                    
+                //     });
+                // });
+
+
+                function addCart(){
+                    ids.push(id);
+                    cant.push();
+
+
+
+                            let contenido = `
+                            <div class="producto${id}">
+                <div class="row g-0">
+                            <li class="list-group-item">${nombre}</li>
+                        </div>
+
+                <div class="row g-0 mb-3">
+                            <div class="btn-group btn-group-sm" role="group" aria-label="btnCantidad"> <!-- col-3 -->
+                                <button type="button" onclick="$('.cantidad${id}').html() <= 1 ? 1 : $('.cantidad${id}').html(parseInt($('.cantidad${id}').html()) - 1);" class="btn btn-dark"><i class="fas fa-minus"></i></button>
+                                <button type="button" class="btn btn-dark cantidad${id}" id="cantidadP">1</button>
+                                <button type="button" onclick="$('.cantidad${id}').html(parseInt($('.cantidad${id}').html()) + 1);" class="btn btn-dark"><i class="fas fa-plus"></i></button>
+                                <button type="button" class="btn btn-danger" onclick="eliminarElemento(${id})"><i class="fas fa-times"></i></button>
+                            </div>
+                        </div>
+                        </div>
+
+                `;
+
+                
+                contenidos.push(contenido);
+                
+                $("#contCont").html(contenidos);
+                let cantidadP = getElementById("cantidadP").value;
+
+                console.log(cantidadP);
+
+                }
+
+
+            });
+
+             function eliminarElemento(idE){
+                
+                $('.producto' + idE).remove();
+                let i = 0;
+                ids.forEach(id => {
+                    if(id == idE){
+                        ids.splice(i,1);
+                        contenidos.splice(i,1);
+
+
+                    }
+
+                    i++;
+                });
+
+                console.log(idE);
+
+
+
+
+
+
+
+             }
+
+
+             // $(document).on('click', '#',function () {
+
+             // });
+
+        </script>
     </body>
 </html>
